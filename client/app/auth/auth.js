92433b24c88388dbb48602bdf1d92d2a48bf08dc
MKS-Student <mks.student@hackreactor.com>
2015-04-04 11:36:01 -0500

    Links to pages, validates forms

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -6,25 +6,39 @@ angular.module('shortly.auth', [])
 .controller('AuthController', function ($scope, $window, $location, Auth) {
   $scope.user = {};
 
+  $scope.invalid = false;
+
   $scope.signin = function () {
-    Auth.signin($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('com.shortly', token);
-        $location.path('/links');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    if ($scope.user.password && $scope.user.username) {
+      $scope.invalid=false;
+      Auth.signin($scope.user)
+        .then(function (token) {
+          $window.localStorage.setItem('com.shortly', token);
+          $location.path('/links');
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+      } else {
+        $scope.invalid=true;
+      }
   };
 
   $scope.signup = function () {
-    Auth.signup($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('com.shortly', token);
-        $location.path('/links');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    if ($scope.user.password && $scope.user.username) {
+      $scope.invalid=false;
+      Auth.signup($scope.user)
+        .then(function (token) {
+          $window.localStorage.setItem('com.shortly', token);
+          $location.path('/links');
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+      } else {
+        $scope.invalid=true;
+      }
+
   };
+
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,18 +2,26 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
 
-  //$scope.invalid = false;
+  $scope.isValidUrl = function(url) {
+    return url.match($scope.rValidUrl);
+  }
+
+  $scope.invalid = false;
 
   $scope.link = {};
   $scope.addLink = function() {
-    Links.addLink($scope.link.url)
-    .then(function(resp){
-      $scope.link ='';
-      if (resp === false){
+    if ($scope.isValidUrl($scope.link.url)) {
+      Links.addLink($scope.link.url)
+      .then(function(resp){
+        $scope.link.url ='';
+        $scope.invalid=false;
+      })
+    } else {
+      $scope.invalid = true;
+    }
 
-      }
-    })
   }
 
 });
